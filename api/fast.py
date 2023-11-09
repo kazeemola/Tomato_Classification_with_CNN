aab932c7f0fe0a57c5250180150ea0ea71bee031
kazeemola <100069681+kazeemola@users.noreply.github.com>
2023-11-09 20:20:51 +0100

    Update fast.py

--- api/fast.py
+++ api/fast.py
@@ -61,5 +61,5 @@ async def predict(
     }
 
 
-#if __name__ == "__main__":
-    #uvicorn.run(app, host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
